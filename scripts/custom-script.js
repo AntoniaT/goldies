6c6abb9c3f6e6445c4d723149ca3c08925e5e36c
Antonia <gintoni@gmx.at>
2020-02-05 19:01:38 +0100

    adding content, modals, forms

--- scripts/custom-script.js
+++ scripts/custom-script.js
@@ -54,10 +54,71 @@ function updateArea(){
 }
 updateArea()
 
-/* let clickedCard = document.querySelector(".cardModal");
+/* Making the Modal on the event search result page display the right card clicked */
 
-clickedCard.addEventListener("click", openModal); */
+let viewBtn1 = document.querySelector("#view1");
+let viewBtn2 = document.querySelector("#view2");
+let viewBtn3 = document.querySelector("#view3");
+let viewBtn4 = document.querySelector("#view4");
+let viewBtn5 = document.querySelector("#view5");
+let viewBtn6 = document.querySelector("#view6");
+let viewBtn7 = document.querySelector("#view7");
 
-function openModal(){
-    alert("You have to be signed in to book this event!");
+const modalTitle = document.querySelector('#modalTitle');
+const modalImg = document.querySelector('#modalImg');
+const modalText = document.querySelector('#modalText');
+const modalOrgan = document.querySelector('#modalOrganizer');
+
+viewBtn1.addEventListener("click", fetchData);
+viewBtn2.addEventListener("click", fetchData);
+viewBtn3.addEventListener("click", fetchData);
+viewBtn4.addEventListener("click", fetchData);
+viewBtn5.addEventListener("click", fetchData);
+viewBtn6.addEventListener("click", fetchData);
+viewBtn7.addEventListener("click", fetchData);
+
+function fetchData(e){
+  if (e.target === viewBtn1){
+    modalTitle.textContent = "Cycling trip";
+    modalImg.src = "images/photos/cycling-ride.png";
+    modalText.textContent = "Connecting the youth with seniors. Through the trishaw rides youth have opportunities to engage with young people in an active setting, allowing you to have meaningful dialogues and them to learn about your past."
+    modalOrgan.textContent = "Cycling Without Age is a volunteer organisation founded in 2012, based in Copenhagen";
+  }
+  else if(e.target === viewBtn2){
+    modalTitle.textContent = "Board Games afternoon";
+    modalImg.src = "images/senioren-spielen.jpg";
+    modalText.textContent = "Connecting people who enjoy playing board games every second Saturday of the month. We have hundreds of games you can choose from and everyone is welcome to join."
+    modalOrgan.textContent = "Multigeneration house is a public house that brings together people of all ages";
+  } 
+    else if(e.target === viewBtn3){
+      modalTitle.textContent = "Computer for beginners";
+      modalImg.src = "images/photos/granny-pc.png";
+      modalText.textContent = "We believe that you're never too old to learn new things! This month we facilitate a small workshop for everyone who is interested in learning about how to use a computer. Don't feel afraid, our teachers are helpful and patient and will start from scratch."
+      modalOrgan.textContent = "The Senior Association was founded in 1976 and has 18.960 members.";
+    }
+   else if(e.target === viewBtn4){
+    modalTitle.textContent = "Gardening together";
+    modalImg.src = "images/gardening.jpg";
+    modalText.textContent = "We usually meet in one of our members garden and work together. We all bring some snacks and the host offers drinks if we get thursty. You should bring practical clothes and shoes that can get dirty."
+    modalOrgan.textContent = "The nature lovers is a bunch of private people who love to be outside and garden in company";
+  }  
+   else if(e.target === viewBtn5){
+    modalTitle.textContent = "Painting class";
+    modalImg.src = "images/photos/senior-painting.png";
+    modalText.textContent = "We are a group of artists and amateurs who just love to paint together. This time we have a portrait class with a real model. Bring your favourite colours and brushes, we have papers that you can use."
+    modalOrgan.textContent = "The school for applied arts is a traditional institution that wants to offer an environment of creativity and atrs to people of all ages and backgrounds.";
+  }  
+    else if(e.target === viewBtn6){
+      modalTitle.textContent = "Sunday hike light";
+      modalImg.src = "images/photos/hiking-goup.png";
+      modalText.textContent = "Do you enjoy taking in fresh air and nature? Then is Sunday hike is for you! We meet in a small group and you don't have to be worries about pace or difficulty. Bring your hiking shoes and comfy clothes."
+      modalOrgan.textContent = "Active Seniors Group is a private association of seniors who enjoy outdoor activities. We started to meet up 4 years ago and made many memories together since.";
+    }  
+    else if(e.target === viewBtn7){
+      modalTitle.textContent = "Baking together";
+      modalImg.src = "images/photos/woman-baking.png";
+      modalText.textContent = "We are a group of ladies that love to bake and meet up to try and test out new recipes. No matter if you are an experienced baker or you just love exploring, we are happy to welcome new members."
+      modalOrgan.textContent = "Our group is just a small circle of ladies who love to chat whilst trying new recipes.";
+    }  
 }
+
